1041e8d6150b258af2fcab0058b38a4fd3e27e55
HayatoTomonari <w81026pc@yahoo.co.jp>
2023-06-12 21:27:20 +0900

    Task:FireBaseの導入。

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:si_proto/welcomePage/welcomePage.dart';
 import 'package:si_proto/welcomePage/ConstantsColor.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
