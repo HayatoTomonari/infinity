3a6fea514237920142386389e593354c7f1aabe4
HayatoTomonari <w81026pc@yahoo.co.jp>
2023-06-18 05:36:29 +0900

    Task:AppBarをグラデーションに変更。

--- lib/pages/change_email.dart
+++ lib/pages/change_email.dart
@@ -34,9 +34,7 @@ class _ChangeEmailState extends State<ChangeEmail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        AppBar(
+        appBar: AppBar(
           title: const Text('メールアドレス変更'),
           centerTitle: true,
           flexibleSpace: Container(
@@ -45,59 +43,61 @@ class _ChangeEmailState extends State<ChangeEmail> {
                     LinearGradient(colors: [Colors.deepPurple, Colors.pink])),
           ),
         ),
-        const SizedBox(height: 48),
-        Padding(
-          padding:
-              const EdgeInsets.only(top: 30, bottom: 10, right: 50, left: 50),
-          child: SizedBox(
-            child: TextField(
-              decoration: const InputDecoration(labelText: '現在のメールアドレス'),
-              enabled: false,
-              controller: TextEditingController(text: user.email),
+        body: Column(
+          children: [
+            const SizedBox(height: 48),
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 30, bottom: 10, right: 50, left: 50),
+              child: SizedBox(
+                child: TextField(
+                  decoration: const InputDecoration(labelText: '現在のメールアドレス'),
+                  enabled: false,
+                  controller: TextEditingController(text: user.email),
+                ),
+              ),
             ),
-          ),
-        ),
-        const SizedBox(height: 48),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-          child: CustomTextField(
-            labelText: 'パスワード',
-            hintText: 'パスワードを入力してください。',
-            obscureText: true,
-            onChangedFunction: (String value) => password = value,
-            icon: Icons.lock_clock_outlined,
-            textColor: ConstantsColor.darkTextColor,
-            focusColor: ConstantsColor.darkFocusColor,
-          ),
-        ),
-        const SizedBox(height: 48),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 50),
-          child: CustomTextField(
-            labelText: '新しいメールアドレス',
-            hintText: '新しいメールアドレス',
-            obscureText: false,
-            onChangedFunction: (String value) => newEmail = value,
-            icon: Icons.mail,
-            textColor: ConstantsColor.darkTextColor,
-            focusColor: ConstantsColor.darkFocusColor,
-          ),
-        ),
-        const SizedBox(height: 48),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 60),
-          child: SizedBox(
-            width: double.infinity,
-            child: CustomButton(
-              labelText: '確認メールを送信',
-              onPressedFunction: () => ConnectionDb.updateEmail(
-                  context, user.email, newEmail, password),
-              textColor: ConstantsColor.darkButtonTextColor,
-              backColor: ConstantsColor.darkButtonBackColor,
+            const SizedBox(height: 48),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+              child: CustomTextField(
+                labelText: 'パスワード',
+                hintText: 'パスワードを入力してください。',
+                obscureText: true,
+                onChangedFunction: (String value) => password = value,
+                icon: Icons.lock_clock_outlined,
+                textColor: ConstantsColor.darkTextColor,
+                focusColor: ConstantsColor.darkFocusColor,
+              ),
             ),
-          ),
-        ),
-      ],
-    ));
+            const SizedBox(height: 48),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 50),
+              child: CustomTextField(
+                labelText: '新しいメールアドレス',
+                hintText: '新しいメールアドレス',
+                obscureText: false,
+                onChangedFunction: (String value) => newEmail = value,
+                icon: Icons.mail,
+                textColor: ConstantsColor.darkTextColor,
+                focusColor: ConstantsColor.darkFocusColor,
+              ),
+            ),
+            const SizedBox(height: 48),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 60),
+              child: SizedBox(
+                width: double.infinity,
+                child: CustomButton(
+                  labelText: '確認メールを送信',
+                  onPressedFunction: () => ConnectionDb.updateEmail(
+                      context, user.email, newEmail, password),
+                  textColor: ConstantsColor.darkButtonTextColor,
+                  backColor: ConstantsColor.darkButtonBackColor,
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
