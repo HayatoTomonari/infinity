16a45bbccfda4d819663bb16b295f6cd6c582762
HayatoTomonari <w81026pc@yahoo.co.jp>
2023-06-18 16:25:53 +0900

    Task:読み込み時の文字化けを解消。

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:si_proto/pages/top_page.dart';
 import 'package:si_proto/pages/welcome_page.dart';
-import 'package:si_proto/utils/constants_color.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:si_proto/utils/constants_text.dart';
 import 'firebase/firebase_options.dart';
@@ -22,13 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: ConstantsText.appTitle,
-      theme: ThemeData.light().copyWith(
-        textTheme: GoogleFonts.bizUDGothicTextTheme(
-          Theme.of(context).textTheme,
-        ),
-        colorScheme: ColorScheme.fromSwatch()
-            .copyWith(secondary: ConstantsColor.lightFocusColor),
-      ),
+      theme: ThemeData(fontFamily: 'NotoSansJP'),
       home: StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
